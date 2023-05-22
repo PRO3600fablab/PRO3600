9be69efe5515c3319ac9ea3cab4eb131b2af6b8e
Ferdinand <ferdinandsellin242002@gmail.com>
2023-05-22 21:16:38 +0200

    linking RDM and SFML

---
+++ src/RDM/headers/assemblage.h
@@ -0,0 +1,7 @@
+#include <vector>
+#include <array>
+#include <cmath>
+#include <valarray>
+#include <Eigen/Dense>
+
+vector<float> assemblage(vector<vector<vector<float>>>matLinkSpec,vector<vector<bool>> matLiaison,vector<bool>U,vector<float>F);
